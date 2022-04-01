b3f61d659ee420cdd0bbf3f2b8acf99b6e6dd0e9
Apozyak <apozyak@yandex.ru>
2022-04-01 22:48:48 +0300

    add random

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 24;
-int NumberB = 13;
-int result = NumberA * NumberB;
+﻿int NumberA = new Random().Next(1,10);
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+int result = NumberA + NumberB;
 Console.WriteLine(result);
