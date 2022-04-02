9bdd7db31fefe8f08930e7f3c43ee3a6087f9473
Apozyak <apozyak@yandex.ru>
2022-04-02 12:10:44 +0300

    add new position

---
+++ Examples005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "карина")
+{
+    Console.Write("Ну наконец-то, рады видеть, ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Ну вот и приперся, не ждали... "); 
+    Console.WriteLine(username);
+}
