66b6d277d95a17bae6b8a4bb8356174775b526d0
Apozyak <apozyak@yandex.ru>
2022-04-02 12:47:36 +0300

    add 006

---
+++ Examples006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 4;
+int c = 6;
+int d = 7;
+int e = 1;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
