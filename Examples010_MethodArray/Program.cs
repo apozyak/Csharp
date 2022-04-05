4e4a257ee7ed6653172e7a8f77f6d0aacc9b3b2a
Apozyak <apozyak@yandex.ru>
2022-04-05 23:41:27 +0300

    add

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 13, 14, 12, 45, 33, 45};
+int n = array.Length;
+int find = 12;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find);
+    {
+    Console.WriteLine(index);
+    }   
+index = index + 1;
+}
